f7252d5eeaba58efde44d709840bf1b910e0643d
b1y9mn <b1y9mn@gmail.com>
2022-10-12 23:41:47 +0900

    변수 수정: request->response

--- netflix/src/api/axios.js
+++ netflix/src/api/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
+  // ? 없어도 알아서 넣어줌!
   params: {
       api_key: "adb01f6c1b0929b51b4cb6942788b2fb",
       language: "ko-KR",
--- netflix/src/components/Banner.js
+++ netflix/src/components/Banner.js
@@ -1,42 +1,46 @@
-  import React, { useEffect, useState } from 'react';
-  import requests from "../api/requests";
-  import axios from "../api/axios";
+//  Banner.js
+import React, { useEffect, useState } from 'react';
+import requests from "../api/requests";
+import axios from "../api/axios";
 
-  const Banner = () => {
+const Banner = () => {
 
-    const [movie, setMovie] = useState([]);
+  const [movie, setMovie] = useState([]);
+  // [] 안의 변수들이 바뀔 때마다 함수가 실행됨(()=>fetchData())
+  // 일단 함수를 한 번 실행 후 [] 안의 변수가 바뀔 때마다 실행
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-      fetchData();
-    }, []);
+  const fetchData = async () => {
+    // 현재 상영중인 영화 정보를 가져오기(여러 영화)
+    // requests는 src/api/requests.js에 있음
+    // pending(보류)는 await로 없앨 수 있음
+    // await을 쓰면 await 뒤에 거를 기다려줌. 받아 올 때까지
+    const response = await axios.get(requests.fetchNowPlaying); 
+    // 콘솔은 많이 찍을 수록 좋음! 나중에는 다 지워야 하지만. 에러를 잡으려고 썼다.
+    // console.log(response);
+    // console.log(response.data.results.length);
+    // console.log(Math.random() * response.data.results.length);
+    // 여러개의 영화 중 하나를 골라서 ID 잡아오기
+    const movieId = 
+    response.data.results[
+        Math.floor(Math.random() * response.data.results.length)
+      ].id;
 
-    // 비동기로 요청하기 때문에 받고 난 다음에 request에 
-    const fetchData = async () => {
-
-      // 현재 상영중인 영화 정보를 가져오기(여러 영화)
-      // requests는 src/api/requests.js에 있음
-      // pending(보류)는 await로 없앨 수 있음
-      const request = await axios.get(requests.fetchNowPlaying); 
-      
-      // 여러개의 영화 중 하나를 골라서 ID 잡아오기
-      const movieId = 
-        request.data.results[
-          Math.floor(Math.random() * requests.data.results.length)
-        ].id;
-
-      // 특정 영화의 더 상세한 정보를 가져오기(비디오 정보도 포함)
-      const results= await axios.get(`movie/${movieId}`, {
-        params: {append_to_response: "videos"},
-      });
-      
-      console.log('results', results);
-    };
-
-    return (
-      <div>
-        
-      </div>
-    );
+    // 특정 영화의 더 상세한 정보를 가져오기(비디오 정보도 포함)
+    const results= await axios.get(`movie/${movieId}`, {
+      params: {append_to_response: "videos"},
+    });
+    
+    console.log('results', results);
   };
 
-  export default Banner;
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Banner;
