cc608a0e744d27850c0e0a211cdb6d85d232a537
biyamn <b1y9mn@gmail.com>
2022-10-14 11:02:03 +0900

    수정: README

--- README.md
+++ README.md
@@ -6,7 +6,8 @@
 ## 2. 폴더 소개
 #### 👉 studylog
 강의를 학습하고 정리하여 studylog 폴더에 업로드
-
+#### 👉 netflix
+강의를 듣고 만든 Netflix 애플리케이션
 
 <br>
 
@@ -18,6 +19,7 @@
 
 #### 2022.10.05 - 1)영화API, axios(+인스턴스), Netflix 구조
 #### 2022.10.06 - 2)Nav 컴포넌트 생성하기, 템플릿 리터럴, `&&`
+#### 2022.10.09 - <a href='https://velog.io/@hamham/%EC%BD%98%EC%86%94%EC%B0%BD%EC%97%90%EC%84%9C-%EC%8B%A4%EC%A0%9C-%EC%BD%94%EB%93%9C-%EB%8C%80%EC%8B%A0-bundle.js%EB%A7%8C-%EB%B3%B4%EC%9D%B4%EB%8A%94-%EB%AC%B8%EC%A0%9C-%ED%95%B4%EA%B2%B0'>콘솔 창에서 실제 코드 대신 bundle.js만 보이는 문제 블로그 포스팅</a>
 #### 2022.10.11 - 3)axios 개념, 비동기, API
 #### 2022.10.12 - 4)Promise, 콜백함수, async&await, axios 실습
 #### 2022.10.13 - 5)JavaScript로 axios 실습
--- netflix/src/components/Banner.js
+++ netflix/src/components/Banner.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import requests from "../api/requests";
 import axios from "../api/axios";
+import "./Banner.css";
 
 const Banner = () => {
 
@@ -33,14 +34,38 @@ const Banner = () => {
     const {data: movieDetail} = await axios.get(`movie/${movieId}`, {
       params: {append_to_response: "videos"},
     });
-    console.log(movieDetail);
+    setMovie(movieDetail);
+  };
+
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n-1) + "..." : str;
   };
 
   return (
-    <div>
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="banner__contents">
+        {/* title이 없으면 name, name이 없으면original_name */}
+        <h1>{movie.title || movie.name || movie.original_name}</h1>
+
+        <div className="banner__buttons">
+          <button className="banner__button play">Play</button>
+          <button className="banner__button info">More Information</button>
+        </div>
+        
+        {/* 설명이 100자 이상이면 100자에서 자르고 ... 출력하기 */}
+        <h1 className="banner__description">{truncate(movie.overview, 100)}</h1>
       
-    </div>
+      </div>
+      <div className="banner__fadeBottom"></div>
+    </header>
   );
-};
+} 
 
 export default Banner;
